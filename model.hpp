1bf4c38231cd5598dc36f4b6c3dc831c5016f52d
eresnoah <eresnoah@gmail.com>
2025-04-26 17:38:04 +0200

    feat: adds model for testing and benchmarking

---
+++ model.hpp
@@ -0,0 +1,46 @@
+#pragma once 
+
+#include "necs.hpp"
+
+using namespace NECS;
+
+// COMPONENTS
+
+struct Health { int value; };
+struct Position { float x; float y; };
+struct Name { std::string value; };
+
+// ARCHETYPES
+
+using A1 = Data<Health>;
+using A2 = Data<Health, Position>;
+using A3 = Data<Health, Name>;
+using A4 = Data<Health, Position, Name>;
+using A5 = Data<Position>;
+using A6 = Data<Position, Name>;
+using A7 = Data<Name>;
+
+using Archetypes = Data<A1, A2, A3, A4, A5, A6, A7>;
+
+// EVENTS
+
+struct AEvent { int value; };
+struct BEvent { int value; };
+
+using Events = Data<AEvent, BEvent>;
+
+// SINGLETONS
+
+struct S1 {};
+struct S2 {};
+struct S3 {};
+
+using Singletons = Data<S1, S2, S3>;
+
+// QUERIES
+
+using SingleQuery = Query<Health>;
+using DoubleQuery = Query<Health, Position>;
+using TripleQuery = Query<Health, Position, Name>;
+
+using Queries = Data<SingleQuery, DoubleQuery, TripleQuery>;
